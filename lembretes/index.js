07a01e1b470210de1822e64d17923246d1e7e309
VictorAraujo03 <victoraraujo.1079@aluno.saojudas.br>
2023-04-07 01:18:01 -0300

    APOSTILA03(MSS):  THUNDER STUFF

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,21 +1,32 @@
-const express = require(`express`)
-const bodyParser = require(`body-parser`)
+const express = require('express');
+const axios = require ('axios')
 const app = express()
-app.use(bodyParser.json())
-const lembretes = {}
-contador = 0
-
-app.get (`/lembretes`, (req, res) => {
-    res.send(lembretes)
-})
-app.post (`/lembretes`, (req, res) => {
-    contador++ //contador = contador +1
-    const { texto } = req.body
-    lembretes[contador] = {
-        contador, texto
+app.use(express.json())
+const lembretes = {};
+contador = 0;
+app.get('/lembretes', (req, res) => {
+  res.send(lembretes);
+});
+app.post('/lembretes', async (req, res) => {
+  contador++;
+  const { texto } = req.body;
+  lembretes[contador] = {
+    contador, texto
+  }
+  await axios.post('http://localhost:10000/eventos', {
+    tipo: 'LembreteCriado',
+    dados:{
+      contador, texto
     }
-    res.status(201).send(lembretes[contador])
+  })
+  res.status(201).send(lembretes[contador]);
+});
+
+app.post('/eventos', (req, res) => {
+  console.log(req.body)
+  res.status(200).send({msg: 'ok'})
 })
-app.listen (4000, () => {
-    console.log(`Lembretes. Porta 4000`)
-})
+
+app.listen(4000, () => {
+  console.log('Lembretes. Porta 4000');
+});
