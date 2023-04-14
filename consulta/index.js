966846129d4eb8ef7a7c856a02287f90b0bd8c54
VictorAraujo03 <victoraraujo.1079@aluno.saojudas.br>
2023-04-13 22:10:24 -0300

    Apostila fim do código 4.3.31

--- consulta/index.js
+++ consulta/index.js
@@ -12,6 +12,11 @@ const funcoes = {
     const observacoes = baseConsulta[observacao.lembreteId]['observacoes'] || []
     observacoes.push(observacao)
     baseConsulta[observacao.lembreteId]['observacoes'] = observacoes
+  },
+  ObservacaoAtualizada: (observacao) => {
+    const observacoes = baseConsulta[observacao.lembreteId]['observacoes']
+    const indice = observacoes.findIndex(o => o.id === observacao.id)
+    observacoes[indice] = observacao
   }
 }
 
@@ -30,7 +35,9 @@ app.post('/eventos', (req, res) => {
       }
     }
   */
+  try { 
     funcoes[req.body.tipo](req.body.dados)
+  } catch (err) {}
     res.status(200).send(baseConsulta)
 })
 
--- observacoes/index.js
+++ observacoes/index.js
@@ -3,6 +3,25 @@ const axios = require('axios')
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 app.use(express.json());
+
+const funcoes = {
+  ObservacaoClassificada: (observacao) => {
+    const observacoes = observacoesPorLembreteId[observacao.lembreteId]
+    const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+    obsParaAtualizar.status = observacao.status
+    const evento = {
+      tipo: "ObservacaoAtualizada",
+      dados: {
+        id: observacao.id,
+        texto: observacao.texto,
+        lembreteId: observacao.lembreteId,
+        status: observacao.status
+      }
+    }
+    console.log(evento)
+    axios.post('http://localhost:10000/eventos', evento)      
+  }
+}
 const observacoesPorLembreteId = {};
 
 //:id é um placeholder
@@ -19,7 +38,7 @@ app.post('/lembretes/:id/observacoes', async (req, res) => {
   await axios.post('http://localhost:10000/eventos', {
     tipo: 'ObservacaoCriada',
     dados: {
-      id: idObs, texto, lembreteId: req.params.id
+      id: idObs, texto, lembreteId: req.params.id, status: 'aguardando'
     }
   })
   res.status(201).send(observacoesDoLembrete);
@@ -31,6 +50,12 @@ app.get('/lembretes/:id/observacoes', (req, res) => {
 
 app.post('/eventos', (req, res) => {
   console.log(req.body)
+  try {    
+    funcoes[req.body.tipo](req.body.dados)
+  }
+  catch (err) {
+    console.log(err)
+  }
   res.status(200).send({msg: 'ok'})
 })
 
